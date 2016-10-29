ab9bf271484e15f8621541e56541ea1c7c72046b
DELL <DELL@HUYHOANG-PC>
2016-10-29 12:48:18 +0700

    Add newsCategory

---
+++ Libraries/Nop.Core/Domain/News/NewsCategory.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Nop.Core.Domain.Discounts;
+using Nop.Core.Domain.Localization;
+using Nop.Core.Domain.Security;
+using Nop.Core.Domain.Seo;
+using Nop.Core.Domain.Stores;
+
+namespace Nop.Core.Domain.News
+{
+    public class NewsCategory : BaseEntity, ISlugSupported, IStoreMappingSupported
+    {
+        private ICollection<NewsItem> _newsItems;
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string MetaKeywords { get; set; }
+
+        public string MetaDescription { get; set; }
+
+        public string MetaTitle { get; set; }
+
+        public int ParentCategoryId { get; set; }
+
+        public int PictureId { get; set; }
+
+        public int PageSize { get; set; }
+
+        public bool AllowCustomersToSelectPageSize { get; set; }
+
+        public string PageSizeOptions { get; set; }
+
+        public string PriceRanges { get; set; }
+
+        public bool ShowOnHomePage { get; set; }
+
+        public bool IncludeInTopMenu { get; set; }
+
+        public bool LimitedToStores { get; set; }
+
+        public bool Published { get; set; }
+
+        public bool Deleted { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreatedOnUtc { get; set; }
+
+        public DateTime UpdatedOnUtc { get; set; }
+        
+        public virtual ICollection<NewsItem> NewsItems
+        {
+            get { return _newsItems ?? (_newsItems = new List<NewsItem>()); }
+            protected set { _newsItems = value; }
+        }
+    }
+}
